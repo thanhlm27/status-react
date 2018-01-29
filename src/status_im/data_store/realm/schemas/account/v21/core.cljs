f7d56c259337f31f7a78560e9d902bdb4104a7d7
alwxndr <alwxndr@gmail.com>
2018-01-29 23:07:11 +0100

    update for migration

--- src/status_im/data_store/realm/schemas/account/v21/core.cljs
+++ src/status_im/data_store/realm/schemas/account/v21/core.cljs
@@ -3,7 +3,7 @@
             [status-im.data-store.realm.schemas.account.v1.chat-contact :as chat-contact]
             [status-im.data-store.realm.schemas.account.v19.contact :as contact]
             [status-im.data-store.realm.schemas.account.v20.discover :as discover]
-            [status-im.data-store.realm.schemas.account.v21.message :as message]
+            [status-im.data-store.realm.schemas.account.v19.message :as message]
             [status-im.data-store.realm.schemas.account.v12.pending-message :as pending-message]
             [status-im.data-store.realm.schemas.account.v1.processed-message :as processed-message]
             [status-im.data-store.realm.schemas.account.v19.request :as request]
@@ -13,7 +13,8 @@
             [status-im.data-store.realm.schemas.account.v8.local-storage :as local-storage]
             [status-im.data-store.realm.schemas.account.v21.browser :as browser]
             [taoensso.timbre :as log]
-            [cljs.reader :as reader]))
+            [cljs.reader :as reader]
+            [clojure.string :as str]))
 
 (def schema [chat/schema
              chat-contact/schema
@@ -37,6 +38,18 @@
     (log/debug "v21 Removing contact " (pr-str contact))
     (.delete new-realm contact)))
 
+(defn remove-location-messages [new-realm old-realm]
+  (let [messages (.objects new-realm "message")]
+    (dotimes [i (.-length messages)]
+      (let [message (aget messages i)
+            content (aget message "content")
+            type    (aget message "content-type")]
+        (when (and (= type "command")
+                   (> (str/index-of content "command=location") -1))
+          (aset message "show?" false))))))
+
 (defn migration [old-realm new-realm]
   (log/debug "migrating v21 account database: " old-realm new-realm)
-  (remove-contact! new-realm "browse"))
+  (-> new-realm
+      (remove-contact! "browse")
+      (remove-location-messages old-realm)))
